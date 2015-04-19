708a21a81add42892528e9cf257b5e2bba51fdcc
Rohit Walavalkar <rhts420@gmail.com>
2015-04-19 13:48:21 +0530

    Added Doxygen comments for the Tree Class.

--- inc/tree.h
+++ inc/tree.h
@@ -36,10 +36,25 @@ class Tree
         list<int> treedata;
     public:
         Tree();
+        /*
+         * Getter function.
+         * */
         Node * get_root();
+        /*
+         * Setter function.
+         * */
         void set_root(Node *root_node);
+        /*
+         * Used for random insertions in tree.
+         * */
         void insert_into_tree();
+        /*
+         * Used for random deletions in tree.
+         * */
         void delete_from_tree();
+        /*
+         * Test functon for writing the tree data to a file.
+         * */
         void print_tree();
 };
 
